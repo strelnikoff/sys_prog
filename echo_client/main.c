152c02975b525839d3daa04973a72fe49cc50855
strelnikoff <strelnikoff@github.com>
2018-01-16 01:46:17 +0300

    эхо клиент сокеты

---
+++ echo_client/main.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <zconf.h>
+
+
+int main()
+{
+    int sock;
+    struct sockaddr_in addr;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8000);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("connect");
+        exit(2);
+    }
+
+    char message[16];
+    char buf[16];
+    scanf("%16s", &message);
+    send(sock, message, sizeof(message), 0);
+    recv(sock, buf, sizeof(message), 0);
+
+    printf(buf);
+    close(sock);
+
+    return 0;
+}
